9d204b2e02ee96acaeba90c925f78c9a696ab5d6
LinuxElectrons <46955586+dip3nluit3l@users.noreply.github.com>
2021-06-26 12:30:14 +0545

    Update xmlrpc.sh

--- xmlrpc.sh
+++ xmlrpc.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
-figlet -f 3d "Linux Electrons" | lolcat -d 2 -a
-echo ""
-echo"************************************************************"
-echo"*            www.facebook.com/linuxelectrons               *"
-echo"*              Youtube.com/linuxelectrons                  *"
-echo"*            github.com/dip3nluit3l/linuxelectron          *"
-echo"************************************************************"
+
 echo "Enter The Url:"
 read url
 wget $url
-
-figlet -f 3d "XMLRPC_TESTER" | lolcat -d 2 -a
-echo ""
-
-echo "Press any key to continue..."
-read_line
 
 isInFile=$(cat index.html | grep -c "xmlrpc.php")
 
